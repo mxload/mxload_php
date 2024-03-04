2dd4256de0ca8e427c29c4ba78930035d667a9cf
takahashi_da_kotaro <kotaro.da.takahashi@drecom.co.jp>
2024-03-04 12:37:30 +0900

    ライブラリ内の初期値を直指定にする

--- config/mxload.php
+++ config/mxload.php
@@ -6,10 +6,10 @@ return [
 |--------------------------------------------------------------------
 */
 
-    'enabled' => env('MXLOAD_ENABLED', false),
-    'collector_url' => env('MXLOAD_COLLECTOR_URL', 'https://lambda-public.mxload.mx/put-request-log'),
-    'project_id' => env('MXLOAD_PROJECT_ID', null),
-    'service_key' => env('MXLOAD_SERVICE_KEY', null),
-    'custom_headers' => env('MXLOAD_CUSTOM_HEADERS', []),
-    'ignore_paths' => env('MXLOAD_IGNORE_PATHS', []),
+    'enabled' => false,
+    'collector_url' => 'https://lambda-public.mxload.mx/put-request-log',
+    'project_id' => null,
+    'service_key' => null,
+    'custom_headers' => [],
+    'ignore_paths' => [],
 ];
